6643f7d1df392c226666364f23442803bda940fb
krasnov-ea <krasnov@stilkuhni.com>
2026-07-20 10:28:27 +0300

    Скрипт Резервного копирования всех баз потаблично

---
+++ backup_All_tables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+BACKUP_DIR="/var/backups/mysql"
+DATE=$(date +%Y%m%d_%H%M%S)
+count=0
+
+mkdir -p "$BACKUP_DIR"
+
+DATABASES=$(mysql -uroot -p'Zxcv#1234' -e "SHOW DATABASES;" -s --skip-column-names)
+
+for DB in $DATABASES; do
+    TABLES=$(mysql -uroot -p'Zxcv#1234' -D "$DB" -e "SHOW TABLES;" -s --skip-column-names)
+   
+    for TABLE in $TABLES; do
+     BACKUP_FILE="$BACKUP_DIR/${DB}_${TABLE}_$DATE.sql"
+       mysqldump -uroot -p'Zxcv#1234' "$DB" "$TABLE" > "$BACKUP_FILE"
+		count=$((count+1))
+    done
+done
+
+echo "Резервное копирование всех таблиц завершено, создано копий: $count"
+echo "Резервные копии находятся в $BACKUP_DIR"
